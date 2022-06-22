cce263672ae2daa91f6c9782cc2de028173fe077
Karan Kapoor <kk8658141@gmail.com>
2022-06-22 16:31:53 +0530

    doosra_college work upto date

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const arr = ['java','c++','c']
+// console.log(arr.length);
+        // first method
+// console.log(arr);
+
+        // second method
+// for(let i = 0 ;i<arr.length; i++) 
+// {
+//     console.log(arr[i]);
+// }
+
+    // third method
+// arr.forEach((element) => {
+//     console.log(element);
+//   });
+    //fourth method
+//   arr.map((karan)=>{
+//     console.log(karan);
+//   })
+
+console.log(new Date());
+console.log(new Date().getTime());
+
---
+++ js/string_functions.js
@@ -0,0 +1,43 @@
+const s = " apple ";
+
+//first 
+
+// console.log(s.substring(0,3));
+
+//second
+
+// console.log(s.indexOf("l"));
+// console.log(s.lastIndexOf("p"));
+
+// third 
+// it is same of substring and it is store the negitive value store 
+
+// console.log(s.slice(0,3));  
+// console.log(s.slice(-3));
+
+// fourth
+                    //start //length
+// console.log(s.substr(1,3));
+
+// fifth
+
+// console.log(s.replace(s,"car"));
+
+//sixth
+
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+
+//seven
+
+// console.log(s.concat(" ", "mango"));
+
+//eight
+
+// console.log(s);
+// console.log(s.trim());
+
+// nine
+const x =12.355;
+console.log(x.toFixed(1));
+
